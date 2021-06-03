91631a89dd9046d14a48654bd5e167235a6d30c7
husnain shahid <84866075+husnain002@users.noreply.github.com>
2021-06-04 01:37:31 +0500

    Update xss.py

--- xss.py
+++ xss.py
@@ -1,3 +1,4 @@
+from numpy import empty
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
@@ -8,33 +9,29 @@ from PIL import Image
 img=Image.open("xss.png")
 st.image(img,width=700)
 st.title("XSS SCANNER")
-
 def get_all_forms(url):
-    """Given a `url`, it returns all forms from the HTML content"""
     soup = bs(requests.get(url).content, "html.parser")
     return soup.find_all("form")
 
 
+
 def get_form_details(form):
-    """
-    This function extracts all possible useful information about an HTML `form`
-    """
+
     details = {}
-    # get the form action (target url)
+    
     action = form.attrs.get("action").lower()
-    # get the form method (POST, GET, etc.)
+    
     method = form.attrs.get("method", "get").lower()
-    # get all the input details such as type and name
+    
     inputs = []
     for input_tag in form.find_all("input"):
         input_type = input_tag.attrs.get("type", "text")
         input_name = input_tag.attrs.get("name")
         inputs.append({"type": input_type, "name": input_name})
-    # put everything to the resulting dictionary
-    details["action"] = action
-    details["method"] = method
-    details["inputs"] = inputs
-    return details
+        details["action"] = action
+        details["method"] = method
+        details["inputs"] = inputs
+        return details
 
 
 def submit_form(form_details, url, value):
@@ -81,8 +78,14 @@ def scan_xss(url):
             
     return is_vulnerable
 
-url = st.text_input("Enter Url here || note ignore error it's an feature ")
-st.write(scan_xss(url))
+empty=""
+url = st.text_input("Enter Url here ")
+if url == empty :
+    st.text("Enter these URL's for Practice purpose.\n \n1: https://xss-game.appspot.com/level1/frame \n2: http://sudo.co.il/xss/level0.php \n3: http://sudo.co.il/xss/level1.php ")
+else:
+    st.write(scan_xss(url))   
+
+st.text("Enter these URL's for Practice purpose.\n \n1: https://xss-game.appspot.com/level1/frame \n2: http://sudo.co.il/xss/level1.php \n3: http://sudo.co.il/xss/level1.php ")
 
 
 
