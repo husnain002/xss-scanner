ed11f9a562f20de7d035fc3fabc333cb7cb77cc3
husnain shahid <84866075+husnain002@users.noreply.github.com>
2021-06-04 12:17:14 +0500

    Update xss.py

--- xss.py
+++ xss.py
@@ -87,6 +87,7 @@ else:
     st.write(scan_xss(url))   
 st.text("Enter these URL's for Practice purpose.\n \n1: https://xss-game.appspot.com/level1/frame \n2: http://sudo.co.il/xss/level0.php \n3: http://sudo.co.il/xss/level1.php ")
 
+st.text(" \n\n\nProject by husnain shahid 🐱‍💻🐱‍🏍")
 
 
 
